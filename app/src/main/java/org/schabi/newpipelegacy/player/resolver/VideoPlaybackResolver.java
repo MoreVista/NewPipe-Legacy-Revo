0fc0ebcd42490da21046aef76f2796b98ad3f5c8
MoreVista <hiro26.1670@gmail.com>
2026-08-22 22:24:14 +0900

    Revert "Play the adaptive formats YouTube now serves as DASH"
    This reverts commit 30c6d6537a2cad5f0a39ea9896b0dac3389b468e.

--- app/src/main/java/org/schabi/newpipelegacy/player/resolver/VideoPlaybackResolver.java
+++ app/src/main/java/org/schabi/newpipelegacy/player/resolver/VideoPlaybackResolver.java
@@ -69,11 +69,10 @@ public class VideoPlaybackResolver implements PlaybackResolver {
         @Nullable final VideoStream video = tag.getSelectedVideoStream();
 
         if (video != null) {
-            final MediaSource streamSource = buildStreamMediaSource(dataSource, video,
-                    PlayerHelper.cacheKeyOf(info, video), tag);
-            if (streamSource != null) {
-                mediaSources.add(streamSource);
-            }
+            final MediaSource streamSource = buildMediaSource(dataSource, video.getUrl(),
+                    PlayerHelper.cacheKeyOf(info, video),
+                    MediaFormat.getSuffixById(video.getFormatId()), tag);
+            mediaSources.add(streamSource);
         }
 
         // Create optional audio stream source
@@ -83,11 +82,10 @@ public class VideoPlaybackResolver implements PlaybackResolver {
         // Use the audio stream if there is no video stream, or
         // Merge with audio stream in case if video does not contain audio
         if (audio != null && (video == null || video.isVideoOnly)) {
-            final MediaSource audioSource = buildStreamMediaSource(dataSource, audio,
-                    PlayerHelper.cacheKeyOf(info, audio), tag);
-            if (audioSource != null) {
-                mediaSources.add(audioSource);
-            }
+            final MediaSource audioSource = buildMediaSource(dataSource, audio.getUrl(),
+                    PlayerHelper.cacheKeyOf(info, audio),
+                    MediaFormat.getSuffixById(audio.getFormatId()), tag);
+            mediaSources.add(audioSource);
         }
 
         // If there is no audio or video sources, then this media source cannot be played back
